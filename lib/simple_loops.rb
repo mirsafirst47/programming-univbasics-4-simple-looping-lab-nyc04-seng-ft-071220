ec174f65a6e6ec4e308173ba6acf42c6dffc830c
Samir Triande <habib.triande@gmail.com>
2020-07-16 10:11:54 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,7 +15,7 @@ def loop_message_n_times(string, integer)
   end
 end
 
-def output_array(array)
+def output_array(array)# output every element of an arrray with the .length
   counter = 0
   while counter < array.length do
     puts array[counter]
@@ -23,7 +23,7 @@ def output_array(array)
   end
 end
 
-def return_string_array(array)
+def return_string_array(array)# convert and return every element of an array as strings.
   counter = 0
   while counter < array.length do
      array[counter] = array[counter].to_s
