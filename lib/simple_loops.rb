eb66e24035c0f70d7238f86648c0614ecb3c0d03
Samir Triande <habib.triande@gmail.com>
2020-07-16 00:24:47 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,33 @@
 # Write your methods here
+def loop_message_five_times(string)
+  counter = 0
+  while counter < 5 do
+    puts string
+    counter += 1
+  end
+end
+
+def loop_message_n_times(string, integer)
+  counter = 0
+  while counter < integer
+    puts string
+    counter += 1
+  end
+end
+
+def output_array(array)
+  counter = 0
+  while counter < array.length do
+    puts array[counter]
+    counter += 1
+  end
+end
+
+def return_string_array(array)
+  counter = 0
+  while counter < array.length do
+     array[counter] = array[counter].to_s
+     counter += 1
+  end
+  array
+end
