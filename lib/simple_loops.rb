098863f179cba3267a8d0e0bc77e7794f2cd3cfb
Samir Triande <habib.triande@gmail.com>
2020-07-16 10:12:40 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -9,13 +9,13 @@ end
 
 def loop_message_n_times(string, integer)
   counter = 0
-  while counter < integer
+  while counter < integer do
     puts string
     counter += 1
   end
 end
 
-def output_array(array)
+def output_array(array)# output every element of an arrray with the .length
   counter = 0
   while counter < array.length do
     puts array[counter]
@@ -23,7 +23,7 @@ def output_array(array)
   end
 end
 
-def return_string_array(array)
+def return_string_array(array)# convert and return every element of an array as strings.
   counter = 0
   while counter < array.length do
      array[counter] = array[counter].to_s
